34a4414175b0992f1150d5887a077caf16f4a522
SYED MINHAJ HUSSAIN <syed.minhaj0911@gmail.com>
2021-10-16 12:32:44 +0530

    refactor

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -11,45 +11,65 @@ export const Navbar = () => {
           fontWeight: "bold",
           color: "red",
         }}
-        style={{ textDecoration: "none" }}
+        style={{
+          textDecoration: "none",
+          color: "#000",
+          backgroundColor: "#fff",
+          padding: "0.2rem 0.5rem",
+          marginRight: "0.25rem",
+        }}
         end
       >
         Home
       </NavLink>{" "}
-      ||
       <NavLink
         to="/brands"
         activeStyle={{
           fontWeight: "bold",
           color: "red",
         }}
-        style={{ textDecoration: "none" }}
+        style={{
+          textDecoration: "none",
+          color: "#000",
+          backgroundColor: "#fff",
+          padding: "0.2rem 0.5rem",
+          marginRight: "0.25rem",
+        }}
       >
         Brands
       </NavLink>{" "}
-      ||
       <NavLink
         to="/men"
         activeStyle={{
           fontWeight: "bold",
           color: "red",
         }}
-        style={{ textDecoration: "none" }}
+        style={{
+          textDecoration: "none",
+          color: "#000",
+          backgroundColor: "#fff",
+          padding: "0.2rem 0.5rem",
+          marginRight: "0.25rem",
+        }}
       >
         Men
       </NavLink>{" "}
-      ||
       <NavLink
         to="/women"
         activeStyle={{
           fontWeight: "bold",
           color: "red",
         }}
-        style={{ textDecoration: "none" }}
+        style={{
+          textDecoration: "none",
+          color: "#000",
+          backgroundColor: "#fff",
+          padding: "0.2rem 0.5rem",
+          marginRight: "0.25rem",
+        }}
       >
         Women
       </NavLink>{" "}
-      ||
     </nav>
   );
 };
